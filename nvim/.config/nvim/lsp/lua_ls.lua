896021824b46a28ff23e3a2e4e153c5e729c1b35
Franklin Grassals <fgrassals@gmail.com>
2025-11-28 18:15:03 -0400

    feat: disable lua_ls telemetry and whole workspace diagnostics for performance

--- nvim/.config/nvim/lsp/lua_ls.lua
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -2,12 +2,16 @@ return {
   settings = {
     Lua = {
       runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+      diagnostics = {
+        workspaceDelay = -1,
+      },
       workspace = {
         -- Don't analyze code from submodules
         ignoreSubmodules = true,
         library = { vim.env.VIMRUNTIME },
         checkThirdParty = false,
       },
+      telemetry = { enable = false },
     },
   },
 }
